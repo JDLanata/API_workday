f352d3bbeb0c6b0c7c75040412e8d8a807701ec0
JDLanata <87950670+JDLanata@users.noreply.github.com>
2021-09-30 21:08:48 -0700

    final push

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # API_workday
+## Description
+-  Create a day planner
+- Made the current time and date appear on top of page using moment
+- Time, Text, and button areas were made using JQUERY and BootStrap
+- text area changes color with time of day
+- text areas can have content saved when save icon is clicked
+
+
+## link to gitHub
+[GitHub](https://github.com/JDLanata/API_workday)
+[GitPages]( https://jdlanata.github.io/API_workday/)
+## Credits
+- [W3 Schools](https://www.w3schools.com/js/default.asp)
+- [Moment](https://momentjs.com/docs/#/query/is-between/)
+- [JQUERY](https://jquery.com/)
+- [BootStrap](https://getbootstrap.com/docs/4.5/getting-started/introduction/)
+
+
+## Screenshot
+![work_api](https://user-images.githubusercontent.com/87950670/135563443-84ae9c9f-19a3-437f-b210-fb0276633f24.png)
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,60 +1,73 @@
 var currentTime;
 
 
+var timeTo = [
+
+    t1 = moment("09:00", "hh:mm").format("kk:mm"),
+    t2 = moment("10:00", "hh:mm").format("kk:mm"),
+    t3 = moment('11:00', "hh:mm").format("kk:mm"),
+    t4 = moment('12:00', "hh:mm").format("kk:mm"),
+    t5 = moment('01:00', "hh:mm").format("kk:mm"),
+    t6 = moment('02:00', "hh:mm").format("kk:mm"),
+    t7 = moment('03:00', "hh:mm").format("kk:mm"),
+    t8 = moment("04:00", "hh:mm").format("kk:mm"),
+    t9 = moment("05:00", "hh:mm").format("kk:mm"),
 
 
-var t1 = $("#t1").text("09:00");
+];
 
+console.log(t1)
+$("#t1").text(t1).addClass("hour")
+$("#t2").text(t2).addClass("hour")
+$("#t3").text(t3).addClass("hour")
+$("#t4").text(t4).addClass("hour")
+$("#t5").text(t5).addClass("hour")
+$("#t6").text(t6).addClass("hour")
+$("#t7").text(t7).addClass("hour")
+$("#t8").text(t8).addClass("hour")
+$("#t9").text(t9).addClass("hour")
 
-
-
-var t2 = $("#t2").text(moment('10:00', 'kk:mm').format("kk:mm"));
-var t3 = $("#t3").text(moment('11:00', 'kk:mm').format("kk:mm"));
-var t4 = $("#t4").text(moment('12:00', 'kk:mm').format("kk:mm"));
-// var t5 = $("#t5").text(moment('20:00', 'kk:mm').format("kk:mm"));
-var t6 = $("#t6").text(moment('14:00', 'kk:mm').format("kk:mm"));
-var t7 = $("#t7").text(moment('15:00', 'kk:mm').format("kk:mm"));
-var t8 = $("#t8").text(moment('16:00', 'kk:mm').format("kk:mm"));
-
-
-var t5 = $("#t5").text("23:00");
+// console.log(JSON.parse($("#t2")))
 
 
 // t1.textContent = moment().format("HH:mm");
 // document.getElementById("toDo").style.backgroundColor = "#d3d3d3";
-        // console.log(description);
+// console.log(description);
 
-var timeTo = [];
 
-function countTime () {
+$('i').addClass("fas fa-save")
+function countTime() {
     console.log("second")
-    currentTime = moment().format("MMMM Do YYYY, kk:mm.ss")
+    currentTime = moment().format("MMMM Do YYYY, hh:mm.ss")
     $("#currentDay").text(currentTime)
-     
+
 }
 setInterval(countTime, 1000)
 
 
 
+// t1.isAfter("currentTime");
+// console.log(t1.isAfter("currentTime"))
 
 var newTime = moment().format("kk:mm");
-for (let i = 1; i < 9; i++) {
-    
-   
-    if ($("#t"+[i]) > newTime){
-        $("#toDo"+[i]).addClass('past');
+console.log(newTime)
+for (let i = 0; i < timeTo.length+1; i++) {
+
+
+    if (timeTo[i] < newTime) {
+        $("#toDo" + [i]).addClass('past');
     }
-    else if ($("#t"+[i]) == newTime){
-        $("#toDo"+ [i]).addClass('present');
+    else if (timeTo[i] == newTime) {
+        $("#toDo" + [i]).addClass('present');
     }
-    else{
-        $("#toDo"+[i]).addClass('future');
+    else {
+        $("#toDo" + [i]).addClass('future');
     }
-    console.log($("#t"+[i]));
+    console.log([i]);
 }
 
 
-    
+
 
 
 
@@ -62,14 +75,14 @@ for (let i = 1; i < 9; i++) {
 //     for( var i=0; localStorage.length; i++){
 //         if(this.id == localStorage.key[i]){
 //             $(this).val(localStorage.getItem(localStorage.key[i]))
-    
-    
+
+
 //         }
 //     }
-    
+
 //     })
 
-    
+
 // var mustDo = localStorage.getItem("toDo")||[];
 
 
@@ -82,112 +95,126 @@ for (let i = 1; i < 9; i++) {
 
 
 
-   
-    var mustDo1 = $(".mustDo1").text(JSON.parse(localStorage.getItem("toDo1")))||[];
-    
-    
-    console.log(mustDo1.val())
-    
-    $("button").click(function(event){
-        event.preventDefault();
-        
-localStorage.setItem("toDo1",JSON.stringify(mustDo1.val()))
+
+var mustDo1 = $(".mustDo1").text(JSON.parse(localStorage.getItem("toDo1"))) || [];
+
+
+console.log(mustDo1.val())
+
+$("button").click(function (event) {
+    event.preventDefault();
+
+    localStorage.setItem("toDo1", JSON.stringify(mustDo1.val()))
 
 })
 
 //////////////////////////////////////////////////////
 
-var mustDo2 = $(".mustDo2").text(JSON.parse(localStorage.getItem("toDo2")))||[];
+var mustDo2 = $(".mustDo2").text(JSON.parse(localStorage.getItem("toDo2"))) || [];
 
 
 console.log(mustDo2.val())
 
-$("button").click(function(event){
-event.preventDefault();
+$("button").click(function (event) {
+    event.preventDefault();
 
-localStorage.setItem("toDo2",JSON.stringify(mustDo2.val()))
+    localStorage.setItem("toDo2", JSON.stringify(mustDo2.val()))
 
 })
 
 
 /////////////////////////////////////
 
-var mustDo3 = $(".mustDo3").text(JSON.parse(localStorage.getItem("toDo3")))||[];
+var mustDo3 = $(".mustDo3").text(JSON.parse(localStorage.getItem("toDo3"))) || [];
 
 
 console.log(mustDo3.val())
 
-$("button").click(function(event){
-event.preventDefault();
+$("button").click(function (event) {
+    event.preventDefault();
 
-localStorage.setItem("toDo3",JSON.stringify(mustDo3.val()))
+    localStorage.setItem("toDo3", JSON.stringify(mustDo3.val()))
 
 })
 
 /////////////////////////////////////
 
-var mustDo4 = $(".mustDo4").text(JSON.parse(localStorage.getItem("toDo4")))||[];
+var mustDo4 = $(".mustDo4").text(JSON.parse(localStorage.getItem("toDo4"))) || [];
 
 
 console.log(mustDo4.val())
 
-$("button").click(function(event){
-event.preventDefault();
+$("button").click(function (event) {
+    event.preventDefault();
 
-localStorage.setItem("toDo4",JSON.stringify(mustDo4.val()))
+    localStorage.setItem("toDo4", JSON.stringify(mustDo4.val()))
 
 })
 /////////////////////////////////////
 
-var mustDo5 = $(".mustDo5").text(JSON.parse(localStorage.getItem("toDo5")))||[];
+var mustDo5 = $(".mustDo5").text(JSON.parse(localStorage.getItem("toDo5"))) || [];
 
 
 console.log(mustDo5.val())
 
-$("button").click(function(event){
-event.preventDefault();
+$("button").click(function (event) {
+    event.preventDefault();
 
-localStorage.setItem("toDo5",JSON.stringify(mustDo5.val()))
+    localStorage.setItem("toDo5", JSON.stringify(mustDo5.val()))
 
 })
 /////////////////////////////////////
 
-var mustDo6 = $(".mustDo6").text(JSON.parse(localStorage.getItem("toDo6")))||[];
+var mustDo6 = $(".mustDo6").text(JSON.parse(localStorage.getItem("toDo6"))) || [];
 
 
 console.log(mustDo6.val())
 
-$("button").click(function(event){
-event.preventDefault();
+$("button").click(function (event) {
+    event.preventDefault();
 
-localStorage.setItem("toDo6",JSON.stringify(mustDo6.val()))
+    localStorage.setItem("toDo6", JSON.stringify(mustDo6.val()))
 
 })
 
 /////////////////////////////////////
 
-var mustDo7 = $(".mustDo7").text(JSON.parse(localStorage.getItem("toDo7")))||[];
+var mustDo7 = $(".mustDo7").text(JSON.parse(localStorage.getItem("toDo7"))) || [];
 
 
 console.log(mustDo7.val())
 
-$("button").click(function(event){
-event.preventDefault();
+$("button").click(function (event) {
+    event.preventDefault();
 
-localStorage.setItem("toDo7",JSON.stringify(mustDo7.val()))
+    localStorage.setItem("toDo7", JSON.stringify(mustDo7.val()))
 
 })
 
 /////////////////////////////////////
 
-var mustDo8 = $(".mustDo8").text(JSON.parse(localStorage.getItem("toDo8")))||[];
+var mustDo8 = $(".mustDo8").text(JSON.parse(localStorage.getItem("toDo8"))) || [];
 
 
 console.log(mustDo8.val())
 
-$("button").click(function(event){
-event.preventDefault();
+$("button").click(function (event) {
+    event.preventDefault();
 
-localStorage.setItem("toDo8",JSON.stringify(mustDo8.val()))
+    localStorage.setItem("toDo8", JSON.stringify(mustDo8.val()))
+
+})
+
+/////////////////////////////////////
+
+var mustDo9 = $(".mustDo9").text(JSON.parse(localStorage.getItem("toDo9"))) || [];
+
+
+console.log(mustDo9.val())
+
+$("button").click(function (event) {
+    event.preventDefault();
+
+    localStorage.setItem("toDo9", JSON.stringify(mustDo9.val()))
 
 })
